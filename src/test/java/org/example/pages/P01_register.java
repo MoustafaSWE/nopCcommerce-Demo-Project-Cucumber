a1c864c14997cae540556a547ec0524c4f885317
Moustafa Ismail <moustafa.ismail@robustastudio.com>
2023-02-17 03:10:47 +0200

    minor change

--- src/test/java/org/example/pages/P01_register.java
+++ src/test/java/org/example/pages/P01_register.java
@@ -11,45 +11,33 @@ import java.time.Duration;
 
 public class P01_register {
 
-    public WebElement clickOnRegisterLink() {
+    public void clickOnRegisterLink() {
         WebElement element = new WebDriverWait(Hooks.driver, Duration.ofSeconds(30))
                 .until(ExpectedConditions.presenceOfElementLocated((By.className("ico-register"))));
 
         Hooks.driver.findElement(By.className("ico-register")).click();
-
-        return Hooks.driver.findElement(By.className("ico-register"));
     }
 
-    public WebElement selectMaleGender() {
+    public void selectMaleGender() {
         WebElement element = new WebDriverWait(Hooks.driver, Duration.ofSeconds(30))
                 .until(ExpectedConditions.presenceOfElementLocated((By.id("gender-male"))));
 
         Hooks.driver.findElement(By.id("gender-male")).click();
-
-        return Hooks.driver.findElement(By.id("gender-male"));
     }
 
     public WebElement enterFirstName(String firstName) {
-        WebElement element = new WebDriverWait(Hooks.driver, Duration.ofSeconds(30))
-                .until(ExpectedConditions.presenceOfElementLocated(By.id("FirstName")));
-
         Hooks.driver.findElement(By.id("FirstName")).sendKeys(firstName);
 
         return Hooks.driver.findElement(By.id("FirstName"));
-
     }
 
     public WebElement enterLastName(String lastName) {
-        WebElement element = new WebDriverWait(Hooks.driver, Duration.ofSeconds(30))
-                .until(ExpectedConditions.presenceOfElementLocated(By.id("LastName")));
-
         Hooks.driver.findElement(By.id("LastName")).sendKeys(lastName);
 
         return Hooks.driver.findElement(By.id("LastName"));
     }
 
     public WebElement selectDay(int day) {
-
         Select select = new Select(Hooks.driver.findElement(By.name("DateOfBirthDay")));
         select.selectByIndex(day);
 
@@ -57,7 +45,6 @@ public class P01_register {
     }
 
     public WebElement selectMonth(int month) {
-
         Select select = new Select(Hooks.driver.findElement(By.name("DateOfBirthMonth")));
         select.selectByIndex(month);
 
@@ -65,7 +52,6 @@ public class P01_register {
     }
 
     public WebElement selectYear(int year) {
-
         Select select = new Select(Hooks.driver.findElement(By.name("DateOfBirthYear")));
         select.selectByIndex(year);
 
@@ -73,34 +59,28 @@ public class P01_register {
     }
 
     public WebElement enterEmail(String email) {
-
         Hooks.driver.findElement(By.id("Email")).sendKeys(email);
 
         return Hooks.driver.findElement(By.id("Email"));
     }
 
     public WebElement enterPassword(String password) {
-
         Hooks.driver.findElement(By.id("Password")).sendKeys(password);
 
         return Hooks.driver.findElement(By.id("Password"));
     }
 
     public WebElement enterConfirmPassword(String password) {
-
         Hooks.driver.findElement(By.id("ConfirmPassword")).sendKeys(password);
 
         return Hooks.driver.findElement(By.id("ConfirmPassword"));
     }
 
     public void clickOnRegisterButton() {
-
         Hooks.driver.findElement(By.name("register-button")).click();
-
     }
 
     public WebElement successMessageText() {
-
         return Hooks.driver.findElement(By.className("result"));
     }
 
